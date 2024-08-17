86aba5f5af4a44d9c287b256b8e809afda55f407
GAURAV SUHANDA <gaurav.suhanda@gmail.com>
2024-08-16 22:40:41 -0400

    Image updated

--- js/script.js
+++ js/script.js
@@ -34,3 +34,29 @@
     $('.social-links a').tooltip();
 
 })(jQuery);
+
+
+// Let's talk function
+
+document.addEventListener('DOMContentLoaded', function() {
+  emailjs.init('WylPwE3s-WVJngTtY'); // Replace with your actual public key from EmailJS
+
+  var form = document.getElementsByClassName('contact-form')[0]; // Select the first form element
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const serviceID = 'service_portfolio_con'; // Replace with your actual service ID from EmailJS
+    const templateID = 'service_portfolio_con'; // Replace with your actual template ID from EmailJS
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('Email successfully sent!');
+      }, function(error) {
+        console.log('FAILED...', error);
+        alert('Email sending failed.');
+      });
+  });
+});
+
